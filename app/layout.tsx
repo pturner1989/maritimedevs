194f9d3f43e616ae691df328d5e32ee9f23c3ed8
Pete <pturner1989@gmail.com>
2025-06-29 12:55:58 +0100

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <Analytics/>
     </html>
   );
 }
